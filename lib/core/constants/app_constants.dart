75068ec72dddf5ac556bef7b7ce7f3144fd47c8c
mohammed reda <reda2o128o1@gmail.com>
2026-06-27 23:25:24 +0300

    hide server IP address

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -2,5 +2,8 @@ class AppConstants {
   static const String currency = 'IQD';
   static int currentUserID = -1;
   static String? currentUserEmail;
-  static const String serverUrl = 'http://192.168.0.196:5036/api';
+
+  Your_Server_IP ---->
+  static const String serverUrl = 'http://Your_Server_IP/api';
+
 }

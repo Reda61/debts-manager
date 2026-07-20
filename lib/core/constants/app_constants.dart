7aaf981a20f2b37c004b8f8833e476d95ab034ea
mohammed reda <reda2o128o1@gmail.com>
2026-07-20 21:43:20 +0300

    update git ignore

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,7 +1,9 @@
+import 'package:expenses4/core/constants/my_server_ip.dart';
+
 class AppConstants {
   static const String currency = 'IQD';
   static int currentUserID = -1;
   static String? currentUserEmail;
 
-  static const String serverUrl = 'YOUR_SERVER_URL_HERE';
+  static String serverUrl = myServerUrl();
 }
